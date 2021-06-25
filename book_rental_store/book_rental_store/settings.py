0c7f31899ce4375c4ccf85665d533f651224b397
Joe Kimlinger <joe.kimlinger@parivedasolutions.com>
2021-06-24 22:51:44 -0500

    Ignore generated static files

--- book_rental_store/book_rental_store/settings.py
+++ book_rental_store/book_rental_store/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zs!k-$5lb-&qg9tcxo7x^)9e0h==r77b3p!ydhw&_jewc4be)h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
